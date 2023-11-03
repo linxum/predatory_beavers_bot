eb12b4d9fc1e50f1ec912ba629e2e8e546ff301c
Ivan Smekchnev <ivansmehnev@yandex.ru>
2023-11-04 02:45:16 +0300

    Added ReplyKeyboard

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ token = "6578454575:AAE9ZgatzU730m4vslDHJqgQu8ayAYsHkDo"
 channel_id = ""
 
 bot = telebot.TeleBot(token)
+keys_menu = types.ReplyKeyboardMarkup(True, True)
+keys_menu.add("Расписание", "Состав", "Написать напутствие")
 
 
 @bot.message_handler(commands=['start'])
@@ -14,10 +16,10 @@ def com_start(message):
     subscribe(message.chat.id)
 
 
-def send_message_on_subscribe(bot, channel_id, user_id):
-    if bot.get_chat_member(channel_id, user_id).status == "member":
-        bot.send_message(user_id, "Привет! Так как ты подписался на наш канал, то мы подумали, что ты бы хотел "
-                                  "попользоваться нашим ботом")
+# def send_message_on_subscribe(bot, channel_id, user_id):
+#     if bot.get_chat_member(channel_id, user_id).status == "member":
+#         bot.send_message(user_id, "Привет! Так как ты подписался на наш канал, то мы подумали, что ты бы хотел "
+#                                   "попользоваться нашим ботом")
 
 
 bot.polling()
