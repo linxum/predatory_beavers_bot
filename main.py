dc6214cdc21093c9191d3beb43c4db3ba230a6a9
Ivan Smekchnev <ivansmehnev@yandex.ru>
2023-11-05 15:55:29 +0300

    Added Inline keyboard for Links and edited check subscribe

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ from telebot import types
 import mailing
 
 token = "6578454575:AAE9ZgatzU730m4vslDHJqgQu8ayAYsHkDo"
-channel_id = "@predatorybeaver"
-
 bot = telebot.TeleBot(token)
+
 keys_menu = types.ReplyKeyboardMarkup(True, True)
 keys_menu.add("Расписание", "Состав", "Написать напутствие")
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Добро пожаловать в бот!")
-    if is_subscribed(channel_id, message.from_user.id):
+    if not is_subscribed("@predatorybeaver", message.from_user.id):
         key_subscribe = types.ReplyKeyboardMarkup(True, True)
         key_subscribe.add("Проверить подписку")
 
@@ -21,13 +19,15 @@ def start(message):
         get_link.add(types.InlineKeyboardButton(text="PREDATORY BEAVERS", url="https://t.me/predatorybeaver"))
 
         bot.send_message(message.chat.id, "Ссылка на канал", reply_markup=get_link)
-        bot.send_message(message.chat.id, "Для работы с ботом вам необходимо подписаться на наш канал!",
-                         reply_markup=key_subscribe)
+        bot.send_message(message.chat.id, "Для работы с ботом вам необходимо подписаться на наш канал!", reply_markup=key_subscribe)
+    else:
+        bot.send_message(message.chat.id, "Добро пожаловать в бот!", reply_markup=keys_menu)
+        mailing.subscribe(message.chat.id)
 
 
-def is_subscribed(channel_id, user_id):
+def is_subscribed(chat_id, user_id):
     try:
-        bot.get_chat_member(channel_id, user_id)
+        bot.get_chat_member(chat_id, user_id)
         return True
     except telebot.apihelper.ApiTelegramException as e:
         if e.result_json['description'] == 'Bad Request: user not found':
@@ -36,16 +36,21 @@ def is_subscribed(channel_id, user_id):
 
 @bot.message_handler(content_types=['text'])
 def keys(message):
-    if message.text == "Проверить подписку":
-        if is_subscribed(channel_id, message.from_user.id):
-            key_subscribe = types.ReplyKeyboardMarkup(True, True)
-            key_subscribe.add("Проверить подписку")
+    match message.text:
+        case "Проверить подписку":
+            start(message)
+        case "Состав":
+            keys_games = types.InlineKeyboardMarkup()
+            keys_games.add(types.InlineKeyboardButton(text="CS2", callback_data='cs2'))
+            keys_games.add(types.InlineKeyboardButton(text="Dota 2", callback_data='dota'))
+            bot.send_message(message.chat.id, "Выбери команду: ", reply_markup=keys_games)
 
-            get_link = types.InlineKeyboardMarkup()
-            get_link.add(types.InlineKeyboardButton(text="PREDATORY BEAVERS", url="https://t.me/predatorybeaver"))
-
-            bot.send_message(message.chat.id, "Ссылка на канал", reply_markup=get_link)
-            bot.send_message(message.chat.id, "Для работы с ботом вам необходимо подписаться на наш канал!", reply_markup=key_subscribe)
-
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    match call.data:
+        case 'cs2':
+            bot.send_message(call.message.chat.id, "CS2")
+        case 'dota':
+            bot.send_message(call.message.chat.id, "Dota")
 
 bot.polling()
