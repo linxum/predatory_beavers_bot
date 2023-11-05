40b097e36e78dbda455e82d0d6f1ff6c14e6a051
Ivan Smekchnev <ivansmehnev@yandex.ru>
2023-11-05 15:42:04 +0300

    Added unsubscribe function

--- mailing.py
+++ mailing.py
@@ -7,3 +7,12 @@ def subscribe(id):
 
         if not flag:
             print(id, file=f)
+
+
+def unsubscribe(id):
+    with open("id.txt", "r") as f:
+        lines = f.readlines()
+    with open("id.txt", "w") as f:
+        for line in lines:
+            if line.strip("\n") != str(id):
+                f.write(line)
