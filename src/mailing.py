d9309f13e2c52d5d970c0c2909d3eb21517ceb12
Ivan Smekchnev <ivansmehnev@yandex.ru>
2023-11-13 19:06:13 +0300

    Remove unsubscribe function

--- src/mailing.py
+++ src/mailing.py
@@ -1,18 +1,11 @@
+import csv
+
 def subscribe(id):
     flag = False
-    with open('resources/id.txt', 'r+') as f:
+    with open('resources/ids.txt', 'r+') as f:
         for line in f:
             if line == str(id) + '\n':
                 flag = True
 
         if not flag:
             print(id, file=f)
-
-
-def unsubscribe(id):
-    with open("resources/id.txt", "r") as f:
-        lines = f.readlines()
-    with open("resources/id.txt", "w") as f:
-        for line in lines:
-            if line.strip("\n") != str(id):
-                f.write(line)
