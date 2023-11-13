e00e70b50ba4b9552ed45c1fc8d6a858353e20df
Ivan Smekchnev <ivansmehnev@yandex.ru>
2023-11-13 19:06:42 +0300

    Added get for schedule.csv

---
+++ src/schedule_games.py
@@ -0,0 +1,16 @@
+import csv
+
+
+def get(bot, message):
+    with open("resources/games.csv", "r") as fileR:
+        reader = csv.DictReader(fileR)
+        for row in reader:
+            msg = "{day} {month} в {hour}:{minute} наша команда будет играть с {enemy} в {game}".format(day=row['day'],
+                                                                                                        month=row['month'],
+                                                                                                        hour=row['hour'],
+                                                                                                        minute=row['minute'],
+                                                                                                        enemy=row['enemy'],
+                                                                                                        game=row['game'])
+            bot.send_message(message.chat.id, msg)
+
+def get_time_game():
