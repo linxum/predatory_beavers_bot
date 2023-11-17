8b4af7d308e18e3701578f530adce7c844d53a70
Ivan Smekchnev <ivansmehnev@yandex.ru>
2023-11-17 16:35:55 +0300

    Edited function names

--- src/players.py
+++ src/players.py
@@ -15,32 +15,30 @@ def get(game):
 
 
 
-def games(message, bot):
+def add_game(message, bot):
     game = bot.send_message(message.chat.id, "game")
-    bot.register_next_step_handler(game, fame, bot)
+    bot.register_next_step_handler(game, add_fname, bot)
 
 
-def fame(message, bot):
+def add_fname(message, bot):
     name = bot.send_message(message.chat.id, "first_name")
-    bot.register_next_step_handler(name, lame, bot, message.text)
+    bot.register_next_step_handler(name, add_lname, bot, message.text)
 
 
-def lame(message, bot, game):
+def add_lname(message, bot, game):
     name = bot.send_message(message.chat.id, "last_name")
-    bot.register_next_step_handler(name, url, bot, game, message.text)
+    bot.register_next_step_handler(name, add_url, bot, game, message.text)
 
 
-def url(message, bot, game, fames):
+def add_url(message, bot, game, f_name):
     ur = bot.send_message(message.chat.id, "url")
-    bot.register_next_step_handler(ur, add_new, game, fames, message.text)
+    bot.register_next_step_handler(ur, add, game, f_name, message.text)
 
 
-def add_new(message, game, fname, lname):
-    player = {'game': game, 'first_name': fname, 'last_name': lname, 'url': message.text}
-    new_player(player)
-
-
-def new_player(player):
+def add(message, bot, game = "", f_name = "", l_name = ""):
+    player = {'game': game, 'first_name': f_name, 'last_name': l_name, 'url': message.text}
     with open("resources/players.csv", "a", newline='\n', encoding='utf-8') as file:
         writer = csv.DictWriter(file, ['game', 'first_name', 'last_name', 'url'])
         writer.writerow(player)
+    bot.send_message(message.chat.id, "Успешно", reply_markup=keys_admin)
+    file.close()
