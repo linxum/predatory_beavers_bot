9938a413eeae65e3525f904509f421a8b4badaa3
Ivan Smekchnev <ivansmehnev@yandex.ru>
2023-11-23 22:37:22 +0300

    first_name and last_name were combined into a separate field

--- src/players.py
+++ src/players.py
@@ -23,33 +23,28 @@ def get_games():
 
 def add(message, bot):
     game = bot.send_message(message.chat.id, "game")
-    bot.register_next_step_handler(game, add_fname, bot)
+    bot.register_next_step_handler(game, add_name, bot)
 
 
-def add_fname(message, bot):
+def add_name(message, bot):
     name = bot.send_message(message.chat.id, "first_name")
     bot.register_next_step_handler(name, add_nick, bot, message.text)
 
 
 def add_nick(message, bot, game):
     nick = bot.send_message(message.chat.id, "nick")
-    bot.register_next_step_handler(nick, add_lname, bot, game, message.text)
+    bot.register_next_step_handler(nick, add_url, bot, game, message.text)
 
 
-def add_lname(message, bot, game, f_name):
-    name = bot.send_message(message.chat.id, "last_name")
-    bot.register_next_step_handler(name, add_url, bot, game, f_name, message.text)
-
-
-def add_url(message, bot, game, f_name, nick):
+def add_url(message, bot, game, name):
     url = bot.send_message(message.chat.id, "url")
-    bot.register_next_step_handler(url, add_player, bot, game, f_name, nick, message.text)
+    bot.register_next_step_handler(url, add_player, bot, game, name, message.text)
 
 
-def add_player(message, bot, game, f_name, nick, l_name):
-    player = {'game': game, 'first_name': f_name, 'nick': nick, 'last_name': l_name, 'url': message.text}
+def add_player(message, bot, game, name, nick):
+    player = {'game': game, 'name': name, 'nick': nick, 'url': message.text}
     with open("resources/players.csv", "a", newline='\n', encoding='utf-8') as file:
-        writer = csv.DictWriter(file, ['game', 'first_name', 'nick', 'last_name', 'url'])
+        writer = csv.DictWriter(file, ['game', 'name', 'nick', 'url'])
         writer.writerow(player)
 
 
