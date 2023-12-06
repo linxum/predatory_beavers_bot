483e0de010f5572a383730460e813021565c1bfd
Ivan Smekchnev <ivansmehnev@yandex.ru>
2023-12-06 13:21:17 +0300

    Fixed bugs

--- src/gift.py
+++ src/gift.py
@@ -7,22 +7,28 @@ path = "C:/Users/smehn/PycharmProjects/Predatory Beavers Bot/gifts"
 
 def add(message, bot):
     with open("gifts/{count}.txt".format(count=len(os.listdir(path)) + 1), 'w+', encoding="utf-8") as fileW:
-        print(f"Отправитель: {message.chat.first_name} {message.chat.last_name}", file=fileW)
+        if message.chat.last_name != None:
+            print(f"Отправитель: {message.chat.first_name} {message.chat.last_name}", file=fileW)
+        else:
+            print(f"Отправитель: {message.chat.first_name}", file=fileW)
         print(message.text, file=fileW)
-    bot.send_message(message.chat.id, "Успешно", reply_markup=keys_menu)
+    bot.send_message(message.chat.id, "Твое сообщение уже доставлено игрокам ❤️‍🔥\n\nБлагодарим!", reply_markup=keys_menu)
 
 
 def get(message, bot):
-    for gift in os.listdir(path):
-        with open(f"gifts/{gift}", "r", encoding="utf-8") as fileR:
-            keys_gift = types.InlineKeyboardMarkup()
-            keys_gift.add(types.InlineKeyboardButton(text="Y", callback_data="gift_yes"))
-            keys_gift.add(types.InlineKeyboardButton(text="N", callback_data="gift_no"))
-            bot.send_message(message.chat.id, fileR.read(), reply_markup=keys_gift)
+    if len(os.listdir(path)) > 0:
+        for gift in os.listdir(path):
+            with open(f"gifts/{gift}", "r", encoding="utf-8") as fileR:
+                keys_gift = types.InlineKeyboardMarkup()
+                keys_gift.add(types.InlineKeyboardButton(text="Y", callback_data="gift_yes"))
+                keys_gift.add(types.InlineKeyboardButton(text="N", callback_data="gift_no"))
+                bot.send_message(message.chat.id, fileR.read(), reply_markup=keys_gift)
+    else:
+        bot.send_message(message.chat.id, "Новых пожеланий нет")
 
 
 def reply_to(message, bot):
-    bot.send_message(message.chat.id, f"{message.text}")
+    bot.send_message(-1002120616869, f"{message.text}")
     remove(message)
 
 
